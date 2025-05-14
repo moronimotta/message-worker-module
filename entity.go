322ff5b1f09b9257df532699427b53dc4f5da6fe
moronimotta <dareevil7@gmail.com>
2025-05-14 14:33:42 -0300

    Event.Data type byte

--- entity.go
+++ entity.go
@@ -3,8 +3,8 @@ package message
 import "encoding/json"
 
 type Event struct {
-	Event string      `json:"event"`
-	Data  interface{} `json:"data"`
+	Event string `json:"event"`
+	Data  []byte `json:"data"`
 }
 
 func (e *Event) Marshal() ([]byte, error) {
